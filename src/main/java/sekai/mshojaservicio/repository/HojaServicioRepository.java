7c0190195061749454158a738bfe0c1b9436137e
Ant29bj <bojorquez29an@gamil.com>
2023-11-28 23:32:49 -0700

    corregido en el jar

--- src/main/java/sekai/mshojaservicio/repository/HojaServicioRepository.java
+++ src/main/java/sekai/mshojaservicio/repository/HojaServicioRepository.java
@@ -1,9 +1,8 @@
 package sekai.mshojaservicio.repository;
 
-import lombok.extern.java.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sekai.mshojaservicio.dto.HojaServicio;
 
-public interface HojaServicioRepository extends JpaRepository<HojaServicio, Log> {
+public interface HojaServicioRepository extends JpaRepository<HojaServicio, Long> {
     public abstract HojaServicio findByNoOrden(String noOrden);
 }
